23106915803673d1d7c40191c019a25fb50c88b1
ThreeSix <ThreeeSix@gmail.com>
2012-04-05 00:52:03 +0300

    Changed the place of build.bat

---
+++ build.bat
@@ -0,0 +1,3 @@
+go build -o ./bin/GameServer.exe ./src/GameServer.go 
+go build -o ./bin/LoginServer.exe ./src/LoginServer.go 
+go build -o ./bin/extractor.exe ./src/extractor.go 
--- src/build.bat
+++
@@ -1,3 +0,0 @@
-go build -o ../bin/GameServer.exe ./GameServer.go 
-go build -o ../bin/LoginServer.exe ./LoginServer.go 
-go build -o ../bin/extractor.exe ./extractor.go 
